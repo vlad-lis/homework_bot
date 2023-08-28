368e73794c539053f5ecbaf52b81cfeec9118f44
vladislav lis <lis.vladislav@gmail.com>
2023-08-28 10:48:49 +0400

    Fixed sending error message; Moved logger setup to main execution

--- homework.py
+++ homework.py
@@ -6,27 +6,16 @@ from http import HTTPStatus
 
 import requests
 import telegram
+from telegram.error import TelegramError
 from dotenv import load_dotenv
 from requests.exceptions import HTTPError, RequestException
 
-from exceptions import TokenError
-
 load_dotenv()
 
-# logger
+# Пробовал делать тут logger = None (или вообще убирать объявление логгера),
+# чтобы уже в main были все настройки,
+# но так не проходят тесты, хотя логгер работает
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-# handler
-handler = logging.StreamHandler(stream=sys.stdout)
-logger.addHandler(handler)
-
-# formatter
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-handler.setFormatter(formatter)
-
 
 PRACTICUM_TOKEN = os.getenv('YA_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TG_TOKEN')
@@ -45,15 +34,7 @@ HOMEWORK_VERDICTS = {
 
 def check_tokens():
     """Проверка наличия токенов и id чата в env."""
-    if (
-        PRACTICUM_TOKEN is None
-        or TELEGRAM_TOKEN is None
-        or TELEGRAM_CHAT_ID is None
-    ):
-        logger.critical('Ошибка при получении токенов или id чата')
-        raise TokenError()
-
-    return True
+    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
 
 
 def send_message(bot, message):
@@ -122,13 +103,14 @@ def parse_status(homework):
 def main():
     """Основная логика работы бота."""
     if not check_tokens():
-        exit(1)
+        logger.critical('Ошибка при получении токенов или id чата')
+        raise SystemExit(1)
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
     previous_status = ''
     current_status = ''
-    error_occurred = None
+    error_message_sent = False
 
     while True:
         try:
@@ -143,23 +125,41 @@ def main():
                 message = parse_status(homework)
                 send_message(bot, message)
                 current_status = homework['status']
+                error_message_sent = False
             else:
                 logger.debug('Статус не изменился')
 
             timestamp = response['current_date']
 
         except Exception as error:
-            error_occurred = True
-            message = f'Сбой в работе программы: {error}'
             logger.error(f'Ошибка: {error}')
 
-            if error_occurred:
-                send_message(bot, error)
-                error_occurred = False
+            if not error_message_sent:
+                message = f'Сбой в работе программы: {error}'
+
+                try:
+                    send_message(bot, error)
+                    error_message_sent = True
+                except TelegramError as tg_error:
+                    logger.error(f'Ошибка TG: {tg_error}')
 
         finally:
             time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
+    # logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    # handler
+    handler = logging.StreamHandler(stream=sys.stdout)
+    logger.addHandler(handler)
+
+    # formatter
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    handler.setFormatter(formatter)
+
     main()
